c8e986dff376f60822067b040699f114ca0f9a59
christian ciceri <christian.ciceri@live.com>
2018-05-31 21:26:40 +0200

    added sync mode

--- src/test-i18n.spec.ts
+++ src/test-i18n.spec.ts
@@ -68,6 +68,7 @@ describe("my first test", () => {
         }
         it("should give the correct literal in SYNC mode", (done) => {
             //load the default language, which is in http://127.0.0.1:8080/assets/i18n/en.json:
+            //loading sync: Christian comment: an example on how monadic solutions are not practical, while coroutines are
             initi18Next();
             //switch the language
             i18n.changeLanguage("en");
